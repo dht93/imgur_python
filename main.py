b559c417b1941fd416996c115f74207824e851d4
Dhruv Thakur <dhruvt93@gmail.com>
2016-01-01 17:27:31 +0530

    re-usability

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 '''Once the database of urls is built using start.py, use this code to download the images'''
 
+import os
 import requests
 import sqlite3
 
-conn=sqlite3.connect('candice.sqlite')                        #database file
+name=raw_input('name?\n')                           #for re-usability
+db_name=name+'.sqlite'
+
+conn=sqlite3.connect(db_name)                        #database file
 cur=conn.cursor()
 
 cur.execute('''
@@ -19,21 +23,26 @@ SELECT url FROM Urls_To_Do WHERE (num>=? and num<?)''',(count,count+10))
 
 urls=[el[0] for el in cur.fetchall()]
 
+directory='D:/Code/PY/Imgur/'+name+'/'                     
+if not os.path.exists(directory):
+    os.path.mkdirs(directory)
+
 for url in urls:
     h=requests.head(url)
     if int(h.headers['content-length'])<1000000:            #making sure the file isn't a gif by making a rough estimate that the
         r=requests.get(url)                                 #content length is less than 1 MB
-        f_name='candice_'+str(count)+'.jpg'
+        f_name=os.path.join(directory, name+'_'+str(count)+'.jpg')
         with open(f_name,'wb') as f:
-                f.write(r.content)
-                f.close()
-                print url
-                cur.execute('''
-                INSERT INTO Urls_Done (num,url) VALUES (?,?)''',(count,url))
-                conn.commit()
+            f.write(r.content)
+            f.close()
+        print url
+        cur.execute('''
+        INSERT INTO Urls_Done (num,url) VALUES (?,?)''',(count,url))
+        conn.commit()
     else:
         print 'gif'
         cur.execute('''
         INSERT INTO Urls_Done (num,url) VALUES (?,?)''',(count,'gif'))
         conn.commit()
     count+=1
+

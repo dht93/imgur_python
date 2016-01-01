6a3959e800e2cab7304e4a225d7ee2553405a557
Dhruv Thakur <dhruvt93@gmail.com>
2016-01-01 17:45:34 +0530

    re-usability

--- main.py
+++ main.py
@@ -29,8 +29,8 @@ if not os.path.exists(directory):
 
 for url in urls:
     h=requests.head(url)
-    if int(h.headers['content-length'])<1000000:            #making sure the file isn't a gif by making a rough estimate that the
-        r=requests.get(url)                                 #content length is less than 1 MB
+    if 20000<int(h.headers['content-length'])<1000000:      #making sure the file isn't a gif by making a rough estimate that the
+        r=requests.get(url)                                 #content length is less than 1 MB. Also discarding images less than 20KB
         f_name=os.path.join(directory, name+'_'+str(count)+'.jpg')
         with open(f_name,'wb') as f:
             f.write(r.content)

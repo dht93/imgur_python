96d3bec0ff8ce54ef9ca772ebffe207d570b1a56
Dhruv Thakur <dhruvt93@gmail.com>
2016-01-01 16:06:24 +0530

    Making use of 2 tables

--- imgur.py
+++ imgur.py
@@ -1,52 +1,39 @@
+'''Once the database of urls is built using start.py, use this code to download the images'''
+
 import requests
-from bs4 import BeautifulSoup
 import sqlite3
 
 conn=sqlite3.connect('candice.sqlite')                        #database file
 cur=conn.cursor()
 
 cur.execute('''
-CREATE TABLE IF NOT EXISTS Urls (url TEXT)''')                
+CREATE TABLE IF NOT EXISTS Urls_Done (num NUMBER, url TEXT)''')
 
 cur.execute('''
-SELECT url FROM URLS''')
+SELECT COUNT(url) FROM Urls_Done''')
 
-l=[r[0].encode() for r in cur.fetchall()]
+count=cur.fetchone()[0]+1
 
-count=len(l) + 1                                              #new batch starts where the last one ended
+cur.execute('''
+SELECT url FROM Urls_To_Do WHERE (num>=? and num<?)''',(count,count+10))
 
-url='http://imgur.com/r/candiceswanepoel'
+urls=[el[0] for el in cur.fetchall()]
 
-r= requests.get(url)
-
-soup=BeautifulSoup(r.text,'html.parser')                     #html parser
-posts=soup.find_all('a','image-list-link')                   #html tag of each image
-
-'''The above method returns only 60 images since the rest of the page is Javascript rendered.
-Noob method to download all images:
-Load the imgur page. Open developer tools and copy the contents of each div element whose class is 'posts sub-gallery br5'.
-Save all this content to a text file in the same folder as the Python file. Comment the previous 3 lines and uncomment the next 
-4 lines. I'm gonna automate this step soon.'''
-
-'''f=open('candice_html.txt')
-soup=BeautifulSoup(f.read(),'html.parser')
-f.close()
-posts=posts=soup.find_all('a','image-list-link')
-'''
-
-for post in posts[:50]:
-    src=post.find('img')['src']
-    u=src[2:]
-    url='http://'+str(u[:-5])+'.jpg'                        #retrieving the url of the full sized file from the url of the thumbnail
-    if not url in l:
-        r=requests.get(url)
+for url in urls:
+    h=requests.head(url)
+    if int(h.headers['content-length'])<1000000:            #making sure the file isn't a gif by making a rough estimate that the
+        r=requests.get(url)                                 #content length is less than 1000000
         f_name='candice_'+str(count)+'.jpg'
         with open(f_name,'wb') as f:
                 f.write(r.content)
                 f.close()
-        cur.execute ('INSERT INTO Urls (url) VALUES (?)',(url,))
-        conn.commit()
-        print count
-        count+=1
+                print url
+                cur.execute('''
+                INSERT INTO Urls_Done (num,url) VALUES (?,?)''',(count,url))
+                conn.commit()
     else:
-        print 'Photo already downloaded'
+        print 'gif'
+        cur.execute('''
+        INSERT INTO Urls_Done (num,url) VALUES (?,?)''',(count,'gif'))
+        conn.commit()
+    count+=1
